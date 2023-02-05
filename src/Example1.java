34fd86571969500bf16bbf1d307e9821e2ac9f01
suresh <sureshbabuk9912@gmail.com>
2023-02-05 20:02:15 +0530

    this is Example1 commit01

---
+++ src/Example1.java
@@ -0,0 +1,15 @@
+/*
+ * 
+ * This is sample project
+ */
+
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello welcome to Example1, statement-1");
+	
+
+	}
+
+}
