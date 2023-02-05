29326ed8703d6fc6a1def573335be345adc21dc9
suresh <sureshbabuk9912@gmail.com>
2023-02-05 22:27:14 +0530

    Example3 Commit1

---
+++ src/Example3.java
@@ -0,0 +1,13 @@
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Example 3, statement-1");
+		System.out.println("Example 3, statement-2");
+		System.out.println("Example 3, statement-3");
+		
+
+	}
+
+}
